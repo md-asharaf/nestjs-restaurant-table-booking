ae3cf4298b3bbd14bcb2b3d66ce3754229025f3f
Md Asharaf <718romanempire@gmail.com>
2025-04-05 03:38:07 +0530

    initialize project structure with NestJS and completed prisma module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(private config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get<string>('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  cleanDatabase() {
+    return this.$transaction([
+      this.reservation.deleteMany(),
+      this.user.deleteMany(),
+      this.restaurant.deleteMany(),
+    ]);
+  }
+}
